c597ae3932309d586644204151054f6e13f7418f
dablaze-ufc <chukwuwauchenna@gmail.com>
2020-09-12 22:47:10 +0100

    refactored code and moved the forgetDialog from the signInFragment

---
+++ app/src/main/java/com/blazingtech/amakasamtv/ui/auth/register/ForgetPasswordDialog.kt
@@ -0,0 +1,117 @@
+package com.blazingtech.amakasamtv.ui.auth.register
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.blazingtech.amakasamtv.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.forgot_password_fragment.*
+import timber.log.Timber
+
+class ForgetPasswordDialog : DialogFragment() {
+
+    private val auth = FirebaseAuth.getInstance()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.forgot_password_fragment, container, false)
+        buttonCancel?.setOnClickListener {
+            this.dismiss()
+        }
+
+        buttonResetPassword?.isEnabled = true
+        buttonResetPassword?.setOnClickListener {
+            if (!validateResetPassword()){
+                returnTransition
+            }else{
+                resetPassword(resetPasswordEmail = textInputFieldForgotPasswordEmail.editText!!.text.toString().trim())
+            }
+        }
+        return view
+    }
+
+    // reset password link code
+    private fun resetPassword(
+        resetPasswordEmail: String
+    ) {
+        showProgressBar()
+        textInputFieldForgotPasswordEmail.isEnabled = false
+        buttonResetPassword?.isEnabled = false
+
+        auth.sendPasswordResetEmail(resetPasswordEmail).addOnSuccessListener {
+            dialogResetEmail()
+            this.dismiss()
+            Timber.i("reset link was sent")
+            removeProgressBarForgotPassword()
+            textInputFieldForgotPasswordEmail.isEnabled = true
+            buttonResetPassword?.isEnabled = true
+            buttonResetPassword?.text = getString(R.string.reset_password)
+
+        }.addOnFailureListener { e ->
+            val snackBar: Snackbar = Snackbar
+                .make(
+                    textInputFieldForgotPasswordEmail,
+                    e.message.toString(),
+                    Snackbar.LENGTH_LONG
+                )
+            snackBar.show()
+
+            removeProgressBarForgotPassword()
+            textInputFieldForgotPasswordEmail.isEnabled = true
+            buttonResetPassword?.isEnabled = true
+            buttonResetPassword?.text = getString(R.string.reset_password)
+        }
+    }
+
+    private fun validateResetPassword(): Boolean {
+         val resetPasswordLink = textInputFieldForgotPasswordEmail.editText!!.text.toString().trim()
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+        return when {
+            (resetPasswordLink.isEmpty()) -> {
+                textInputFieldForgotPasswordEmail.error = "please enter an email!"
+                false
+            }
+            (!resetPasswordLink.matches(emailPattern.toRegex())) -> {
+                textInputFieldForgotPasswordEmail.error = "invalid email address!"
+                false
+            }
+            else -> {
+                textInputFieldForgotPasswordEmail.error = null
+                textInputFieldForgotPasswordEmail.isErrorEnabled = false
+                true
+            }
+        }
+    }
+
+    private fun dialogResetEmail() {
+        val alertDialog = activity?.let { MaterialAlertDialogBuilder(it) }
+
+        alertDialog?.setTitle("Password reset link Sent")
+        alertDialog?.setIcon(R.drawable.ic_reset)
+        alertDialog?.setMessage("please check your Email Inbox to reset your \npassword through the link")
+        alertDialog?.setPositiveButton(
+            "Ok"
+        ) { _, _ -> }
+        alertDialog?.show()
+    }
+
+    private fun showProgressBar() {
+        Timber.i("setUpProgressBarForgotPassword: Started")
+        progressBarForgotPassword?.visibility = View.VISIBLE
+    }
+
+
+    // Removed progress bar forgot password
+    private fun removeProgressBarForgotPassword() {
+        Timber.i("removedProgressBarForgotPassword: gone")
+        progressBarForgotPassword?.visibility = View.GONE
+    }
+
+}
--- app/src/main/java/com/blazingtech/amakasamtv/ui/auth/register/SignInFragment.kt
+++ app/src/main/java/com/blazingtech/amakasamtv/ui/auth/register/SignInFragment.kt
@@ -1,6 +1,5 @@
 package com.blazingtech.amakasamtv.ui.auth.register
 
-import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,19 +7,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.blazingtech.amakasamtv.R
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.forgot_password_fragment.*
-import kotlinx.android.synthetic.main.forgot_password_fragment.view.*
-import kotlinx.android.synthetic.main.forgot_password_fragment.view.buttonResetPassword
 import kotlinx.android.synthetic.main.sign_in_fragment.*
 import kotlinx.android.synthetic.main.sign_in_fragment.view.*
 import timber.log.Timber
@@ -28,17 +22,13 @@ import timber.log.Timber
 class SignInFragment : Fragment() {
 
 
-    private lateinit var alertDialog: AlertDialog
-
-    private lateinit var viewModel: SignInViewModel
+    private  val viewModel: SignInViewModel by viewModels()
     private lateinit var editTextFieldEmailSignIn: TextInputLayout
     private lateinit var editTextFieldPasswordSignIn: TextInputLayout
-    private lateinit var editTextFieldResetPassword: TextInputLayout
 
     // strings
     private lateinit var emailSignIn: String
     private lateinit var passwordSignIn: String
-    private lateinit var resetPasswordLink: String
 
 
     override fun onCreateView(
@@ -46,7 +36,6 @@ class SignInFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.sign_in_fragment, container, false)
-        viewModel = ViewModelProvider(this).get(SignInViewModel::class.java)
 
         setUpFireBaseAuth()
         view.apply {
@@ -56,7 +45,7 @@ class SignInFragment : Fragment() {
                     .navigate(R.id.action_signInFragment_to_signUpFragment)
             }
             findViewById<TextView>(R.id.textViewForgotYourPassword).setOnClickListener {
-                openDialog()
+               ForgetPasswordDialog().show(parentFragmentManager, "Forget password")
             }
             buttonSignIn?.isEnabled = true
             buttonSignIn?.setOnClickListener {
@@ -74,65 +63,6 @@ class SignInFragment : Fragment() {
         editTextFieldEmailSignIn = view.textInputFieldSignInEmail
         editTextFieldPasswordSignIn = view.textInputFieldSignInPassword
         return view
-    }
-
-
-    // Dialog fragment for forgot password
-    private fun openDialog() {
-        val dialogView =
-            LayoutInflater.from(activity).inflate(R.layout.forgot_password_fragment, null)
-        editTextFieldResetPassword = dialogView.textInputFieldForgotPasswordEmail
-        val builder = AlertDialog.Builder(activity)
-            .setView(dialogView)
-
-        alertDialog = builder.show()
-
-        dialogView.buttonCancel?.setOnClickListener {
-            alertDialog.dismiss()
-        }
-        dialogView.buttonResetPassword?.isEnabled = true
-        dialogView.buttonResetPassword?.setOnClickListener {
-            if (!validateResetPassword()) {
-                returnTransition
-            } else {
-                resetPassword(resetPasswordEmail = resetPasswordLink)
-            }
-        }
-
-
-    }
-
-    // reset password link code
-    private fun resetPassword(
-        resetPasswordEmail: String
-    ) {
-        showProgressBar()
-        editTextFieldResetPassword.isEnabled = false
-        buttonResetPassword?.isEnabled = false
-
-        auth.sendPasswordResetEmail(resetPasswordEmail).addOnSuccessListener {
-            dialogResetEmail()
-            alertDialog.dismiss()
-            Timber.i("reset link was sent")
-            removeProgressBarForgotPassword()
-            editTextFieldResetPassword.isEnabled = true
-            buttonResetPassword?.isEnabled = true
-            buttonResetPassword?.text = getString(R.string.reset_password)
-
-        }.addOnFailureListener { e ->
-            val snackBar: Snackbar = Snackbar
-                .make(
-                    editTextFieldResetPassword,
-                    e.message.toString(),
-                    Snackbar.LENGTH_LONG
-                )
-            snackBar.show()
-
-            removeProgressBarForgotPassword()
-            editTextFieldResetPassword.isEnabled = true
-            buttonResetPassword?.isEnabled = true
-            buttonResetPassword?.text = getString(R.string.reset_password)
-        }
     }
 
 
@@ -171,33 +101,6 @@ class SignInFragment : Fragment() {
                 }
 
             })
-
-//                if (auth.currentUser != null) {
-//                    if (!isUserVerified(auth.currentUser!!)) {
-//
-//                    } else {
-////                          TODO("Navigation")
-////                          TODO("Show the appBar and the Bottom navigation")
-//                        Toast.makeText(activity, "Authentication Successful", Toast.LENGTH_LONG)
-//                            .show()
-//                        removeProgressBar()
-//                        disableAllViews(true)
-//                        buttonSignIn?.text = getString(R.string.login)
-//                    }
-//                }
-
-//            }.addOnFailureListener(it) { e ->
-//                val snackBar: Snackbar = Snackbar
-//                    .make(
-//                        constraintLayoutSignIn,
-//                        e.message.toString(),
-//                        Snackbar.LENGTH_LONG
-//                    )
-//                snackBar.show()
-//                removeProgressBar()
-//                disableAllViews(true)
-//                buttonSignIn?.text = getString(R.string.login)
-//            }
 
         }
     }
@@ -271,25 +174,6 @@ class SignInFragment : Fragment() {
         }
     }
 
-    private fun validateResetPassword(): Boolean {
-        resetPasswordLink = editTextFieldResetPassword.editText!!.text.toString().trim()
-        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
-        return when {
-            (resetPasswordLink.isEmpty()) -> {
-                editTextFieldResetPassword.error = "please enter an email!"
-                false
-            }
-            (!resetPasswordLink.matches(emailPattern.toRegex())) -> {
-                editTextFieldResetPassword.error = "invalid email address!"
-                false
-            }
-            else -> {
-                editTextFieldResetPassword.error = null
-                editTextFieldResetPassword.isErrorEnabled = false
-                true
-            }
-        }
-    }
 
     // verifying if user has check link
     private fun isUserVerified(user: FirebaseUser): Boolean {
@@ -314,12 +198,6 @@ class SignInFragment : Fragment() {
         progressBarForgotPassword?.visibility = View.VISIBLE
     }
 
-    // Removed progress bar forgot password
-    private fun removeProgressBarForgotPassword() {
-        Timber.i("removedProgressBarForgotPassword: gone")
-        progressBarForgotPassword?.visibility = View.GONE
-    }
-
     // disable all views
     private fun disableAllViews(state: Boolean) {
         editTextFieldEmailSignIn.isEnabled = state
@@ -330,15 +208,5 @@ class SignInFragment : Fragment() {
         buttonSignIn?.text = ""
     }
 
-    private fun dialogResetEmail() {
-        val alertDialog = activity?.let { MaterialAlertDialogBuilder(it) }
 
-        alertDialog?.setTitle("Password reset link Sent")
-        alertDialog?.setIcon(R.drawable.ic_reset)
-        alertDialog?.setMessage("please check your Email Inbox to reset your \npassword through the link")
-        alertDialog?.setPositiveButton(
-            "Ok"
-        ) { dialog, which -> }
-        alertDialog?.show()
-    }
 }
